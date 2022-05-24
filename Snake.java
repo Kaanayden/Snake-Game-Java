aa6ee00d3d390d3925e6ff68a23cb5e8640f0cf5
Kaanayden <kaanayden15@gmail.com>
2022-05-24 19:58:48 +0300

    fixes

--- Snake.java
+++ Snake.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Snake {
  //direction
  //pixel coordinates (arraylist)
@@ -6,7 +8,18 @@ public class Snake {
 //eatApple()
 //isDead( obstacles ) , snake
 
+private ArrayList<Coordinates> coordinates;
+private Direction direction;
+
+
 public Snake( int length, Coordinates headPosition, Direction direction ) {
+
+    coordinates.add( headPosition );
+    this.direction = direction;
+    for( int i = 1; i < length; i++ ) {
+        coordinates.add(  );
+    }
+    
 
 }
 
